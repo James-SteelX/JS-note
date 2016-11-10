e485207a9f817e88a2a6158d03ef1f99a8fd9efb
James <James.Steel@me.com>
2016-11-10 10:14:05 +0000

    making a start on day 4

--- src/note-controller.js
+++ src/note-controller.js
@@ -5,6 +5,27 @@
       this.noteListView = new NoteListView(this.noteList);
     }
 
+    changeNote();
+
+    function changeNote() {
+      window.addEventListener("hashchange", showNoteOnPage);
+    };
+
+    function showNoteOnPage() {
+      showNote(getNoteFromUrl(window.location));
+    };
+
+    function getNoteFromUrl(location) {
+      var urlID = location.hash.split("#notes/")[1];
+      return this.noteListView.list.notes[urlID].getText();
+    };
+
+    function showNote(note) {
+      document
+        .getElementById("note")
+        .innerHTML = note;
+    };
+
     NoteController.prototype.generateHTML = function () {
       return this.noteListView.getHTML();
     };
